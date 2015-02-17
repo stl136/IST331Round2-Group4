ea541e0fca6fb598acfeb6da9b31107ba126afa9
stlarar <stlarar@client-75-102-86-211.mobility-up.psu.edu>
2015-02-16 19:10:41 -0500

    new changes

---
+++ SpaceInvader/src/spaceinvader/SpaceInvader.java
@@ -0,0 +1,23 @@
+package spaceinvader;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class SpaceInvader 
+{
+
+    public static void main(String[] args) 
+    {
+        JButton blogin = new JButton("Login");
+    JPanel panel = new JPanel();
+    JTextField txuser = new JTextField(15);
+    JPasswordField pass = new JPasswordField(15);
+    JLabel userName = new JLabel("Username");
+    JLabel pin = new JLabel("User Pin");
+
+    }
+    
+}
