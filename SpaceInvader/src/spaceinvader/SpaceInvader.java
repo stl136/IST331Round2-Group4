99e4d096dc62c40bf52411c355454694ecf3a5b8
stlarar <stlarar@client-75-102-86-211.mobility-up.psu.edu>
2015-02-16 20:41:13 -0500

    added buttons

--- SpaceInvader/src/spaceinvader/SpaceInvader.java
+++ SpaceInvader/src/spaceinvader/SpaceInvader.java
@@ -5,12 +5,7 @@ public class SpaceInvader
 
     public static void main(String[] args) 
     {
-
-        //Liam Neigh
-        //Stephanie Larar
-        //Cody Hubley
          myJFrame mjf = new myJFrame();
-        System.out.println("new stuff");
     }
     
 }
--- SpaceInvader/src/spaceinvader/mainMenu.java
+++ SpaceInvader/src/spaceinvader/mainMenu.java
@@ -1,8 +1,40 @@
 
 package spaceinvader;
 
-public class mainMenu 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+public class mainMenu extends myJPanel
 {
-    //menu menu
+    JButton start, exit, character1, character2, target1, target2, background1, background2;
+    JLabel label1, label2, label3, label4;
+    JSlider slider;
+    JPanel mjp;
    
+    public mainMenu()
+    {
+        
+            mjp = new JPanel();
+            
+        
+            start = new JButton("Start the game");
+            exit = new JButton("Exit");
+            
+            label1 = new JLabel("Choose a character");
+            label2 = new JLabel("Choose a target");
+            label3 = new JLabel("Choose a background");
+            label4 = new JLabel("Choose Difficulty");
+            
+            slider = new JSlider();
+            
+            mjp.add(start);
+            mjp.add(exit);
+            mjp.add(label1);
+            mjp.add(label2);
+		
+    }
+    
+    
 }
