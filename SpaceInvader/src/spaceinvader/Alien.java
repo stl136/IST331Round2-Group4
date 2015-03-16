7dda965a74374423cfe41e52a309d3676f4b73b9
Cody <Cody@75.102.87.42>
2015-03-16 18:39:08 -0400

    Shots move

--- SpaceInvader/src/spaceinvader/Alien.java
+++ SpaceInvader/src/spaceinvader/Alien.java
@@ -9,13 +9,37 @@ import javax.swing.JLabel;
 
     int alienX, alienY; 
     boolean exists;
+    boolean moveRight = true;
 
     public Alien(int alienX, int alienY, boolean exists)
     {
         this.alienX = alienX;
         this.alienY = alienY;
         this.exists = exists;
+       
 
+    }
+    
+    public void isShot(){
+        exists = false;
+        
+    }
+    
+    public void move(){
+        if(alienX < 1200 && moveRight == true)
+            alienX += 10;
+        else if(alienX == 1200 && moveRight == true){
+            alienY += 100;
+            moveRight = false;
+        }
+            
+        else if (alienX > 100 && moveRight == false){
+            alienX -= 10;
+        }
+        else if(alienX == 100 && moveRight == false){
+            alienY += 100;
+            moveRight = true;
+        }
     }
 
 }
